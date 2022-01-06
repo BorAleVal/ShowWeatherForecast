9149d12294f4338bb9ef59363b2970cc11fbadcc
Александр Бородин <borodin1985@yandex.ru>
2022-01-06 19:15:11 +0500

    Добавление проекта с тестами

---
+++ ShowWeatherForecastTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using ShowWeatherForecast.Models;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace ShowWeatherForecastTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // TODO : здесь конечно нужно еще тесты сделать, но их я уже не успеваю.
+            Assert.Pass();
+        }
+    }
+}
